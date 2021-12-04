eac678d96c95a11a4785865097bb144a2f2142fb
baalajimaestro <me@baalajimaestro.me>
2021-12-04 19:45:10 +0530

    Adapt for drone
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- push.sh
+++ push.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+DRONE_DIR=$(pwd)
 mkdir /public
 cd /public
 git init
 git remote add origin https://baalajimaestro:${GITEA_TOKEN}@git.baalajimaestro.me/baalajimaestro/personal-website.git
 
 echo -e "Deploying updates to GitHub..."
-cd /app
+cd $DRONE_DIR
 hugo mod get -u
 hugo --gc --minify -d /public
 
