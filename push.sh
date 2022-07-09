631cdd783e2c1545cbc80bbe52d25f434ff24930
baalajimaestro <me@baalajimaestro.me>
2022-07-09 19:37:24 +0530

    Move to gitlab ci
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-DRONE_DIR=$(pwd)
+BASE_DIR=$(pwd)
 mkdir /public
 cd /public
 git init
 git remote add origin https://baalajimaestro:${GITEA_TOKEN}@git.baalajimaestro.me/baalajimaestro/personal-website.git
 
 echo -e "Deploying updates to GitHub..."
-cd $DRONE_DIR
+cd $BASE_DIR
 hugo mod get -u
 hugo --gc --minify -d /public
 
